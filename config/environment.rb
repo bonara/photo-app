ba3ea6ed40567f50e7f3589d47e8cbe941e6456e
Nara Bat-Ochir <naraa_520@yahoo.com>
2016-05-12 20:49:44 +0000

    COnfig update

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgri.net',
+    :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plan,
     :user_name => ENV['useradmin'],
