ba96607d5165959425d441e850918fe37fdfd757
Nara Bat-Ochir <naraa_520@yahoo.com>
2016-05-12 20:39:03 +0000

    Confirmation email function added

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgri.net',
+    :port => '587',
+    :authentication => :plan,
+    :user_name => ENV['useradmin'],
+    :password => ENV['rubyonrails101'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+}
