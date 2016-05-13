69c4f6af393913b63d53e1680fc0507272c08343
Nara Bat-Ochir <naraa_520@yahoo.com>
2016-05-13 05:02:51 +0000

    Authentication update

--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,9 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
-    :authentication => :plan,
-    :user_name => ENV['useradmin'],
-    :password => ENV['rubyonrails101'],
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
 }
